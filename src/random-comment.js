66c875258029431c03723111bc5c9a027c1beff8
iarry <larryrubin93@gmail.com>
2015-09-16 13:33:41 -0600

    Randomized comments

---
+++ src/random-comment.js
@@ -0,0 +1,7 @@
+var comments = ["Don’t you want your child eating this for school lunch? Join me and @Chef Ann Foundation in the fight for #realschoolfood. Realschoolfood.org", "It’s time people were proud of what’s on their lunch tray. Here’s my  #realschoolfood tray, via @ChefAnnFnd. Choose yours at realschoolfood.org.","School lunches should get real. Here’s my idea of #realschoolfood, via @ChefAnnFnd what’s yours? Choose your tray at realschoolfood.org.","School lunch haters need a re-education, because this is what #realschoolfood looks like. Choose your tray at realschoolfood.org!","It’s time school lunches got better grades. #realschoolfood like this deserves an A+. Choose your tray at realschoolfood.org.","When #realschoolfood looks this good, why would anyone go back to old standards? Choose your tray at realschoolfood.org.","Wishing I could make food as good as #realschoolfood. Teach me, @ChefAnnFnd! Realschoolfood.org","When #realschoolfood looks this good… You’re gonna need a bigger tray. Choose your tray via @Chef Ann Foundation at realschoolfood.org","It’s time to stand up for our children’s health and serve them #realschoolfood like this. Choose your tray via @ChefAnnFnd at realschoolfood.org."];
+
+var commentElements = document.getElementsByClassName("lunchDetails-comment");
+
+for( comment in  commentElements){
+    commentElements[comment].innerHTML = comments[Math.floor((Math.random() * commentElements.length))];
+}
